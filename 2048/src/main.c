4d25a464aadefc01c9d2983952b0f07a2d0ae8a6
dogasp <dorian.gaspar95@free.fr>
2020-05-06 16:42:03 +0200

    modifs make windows

--- 2048/src/main.c
+++ 2048/src/main.c
@@ -5,16 +5,12 @@
 int main(int argc, char** argv)
 {
 
-    SDL_Window* pWindow = NULL;
-    pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                SDL_WINDOWPOS_UNDEFINED,
-                                                                640,
-                                                                480,
-                                                                SDL_WINDOW_SHOWN);
+    SDL_Window* window = NULL;
+    window = SDL_CreateWindow("Jeu 2048",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
 
     SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
 
-    SDL_DestroyWindow(pWindow);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
