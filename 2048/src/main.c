67d53b5433465f99093a0664efb6e1e837806e10
dogasp <dorian.gaspar95@free.fr>
2020-05-18 11:49:13 +0200

    started

--- 2048/src/main.c
+++ 2048/src/main.c
@@ -1,5 +1,4 @@
 #include <SDL2/SDL.h>
-
 #include <stdio.h>
 
 int main(int argc, char** argv)
@@ -8,7 +7,20 @@ int main(int argc, char** argv)
     SDL_Window* window = NULL;
     window = SDL_CreateWindow("Jeu 2048",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
 
-    SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer){
+        SDL_PollEvent(&event);
+        switch (event.type){
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+        }
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
